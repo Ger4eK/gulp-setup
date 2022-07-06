0b2d19ccffcfd9dcb79569247c9ddeae10dba89f
Ger4eK <ger4ek228@gmail.com>
2022-07-06 12:51:07 +0300

    const paths

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,17 @@ const gulp = require('gulp');
 const less = require('less');
 const del = require('del');
 
+const paths = {
+  styles: {
+    src: 'src/styles/**/*.less',
+    dest: 'dist/css/',
+  },
+  scripts: {
+    src: 'src/scripts/**/*.js',
+    dest: 'dist/js/',
+  },
+};
+
 const clean = () => {
   return del(['dist']);
 };
